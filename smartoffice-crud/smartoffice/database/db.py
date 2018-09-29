deebb34d1a94ec011fcde48493f5b9f1ebb8a5be
bampatra <s3549111@student.rmit.edu.au>
2018-09-29 20:22:13 +1000

    Personal calendar for each doctor

--- smartoffice-crud/smartoffice/database/db.py
+++ smartoffice-crud/smartoffice/database/db.py
@@ -20,15 +20,19 @@ ma = Marshmallow(app)
 class Patient(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=True)
+    phone = db.Column(db.String(80), unique=True)
+    birthday = db.Column(db.Date, unique=False)
     email = db.Column(db.String(120), unique=True)
     
-    def __init__(self, name, email):
+    def __init__(self, name, phone, birthday, email):
         self.name = name
+        self.phone = phone
+        self.birthday = birthday
         self.email = email
 
 class PatientSchema(ma.Schema):
     class Meta:
-        fields = ('name','email')
+        fields = ('name','phone','birthday','email')
     
 patient_schema = PatientSchema()
 patients_schema = PatientSchema(many = True)
@@ -38,15 +42,17 @@ class Doctor(db.Model):
     name = db.Column(db.String(80), unique=True)
     major = db.Column(db.String(120), unique=False)
     email = db.Column(db.String(120), unique=True)
+    calendar_id = db.Column(db.String(120), unique=True)
 
-    def __init__(self, name, major, email):
+    def __init__(self, name, major, email, calendar_id):
         self.name = name
         self.major = major
         self.email = email
+        self.calendar_id = calendar_id
 
 class DoctorSchema(ma.Schema):
     class Meta:
-        fields = ('name','major','email')
+        fields = ('name','major','email', 'calendar_id')
 
 doctor_schema = DoctorSchema()
 doctors_schema = DoctorSchema(many = True)
@@ -69,22 +75,24 @@ clerks_schema = ClerkSchema(many = True)
 
 class Appointment(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    doctor_id = db.Column(db.Integer, unique = False)
-    patient_id = db.Column(db.Integer, unique = False)
-    date = db.Column(db.Date)
-    time_start = db.Column(db.Time)
-    time_end = db.Column(db.Time)
+    doctor_id = db.Column(db.Integer, unique=False)
+    date = db.Column(db.Date, unique=False)
+    time_start = db.Column(db.Time, unique=False)
+    time_end = db.Column(db.Time, unique=False)
+    patient_id = db.Column(db.Integer, unique=False)
+    event_id = db.Column(db.String(120), unique=False)
 
-    def __init__(self, doctor_id, patient_id, date, time_start, time_end):
+    def __init__(self, doctor_id, date, time_start, time_end, patient_id, event_id):
         self.doctor_id = doctor_id
-        self.patient_id = patient_id
         self.date = date
         self.time_start = time_start
         self.time_end = time_end
+        self.patient_id = patient_id
+        self.event_id = event_id
 
 class AppointmentSchema(ma.Schema):
     class Meta:
-        fields = ('doctor_id','patient_id','date', 'time_start','time_end')
+        fields = ('doctor_id','date','time_start','time_end', 'patient_id', 'event_id')
 
 appointment_schema = AppointmentSchema()
 appointments_schema = AppointmentSchema(many = True)
@@ -93,7 +101,7 @@ class MedicalRecord(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     doctor_id = db.Column(db.Integer, unique = False)
     patient_id = db.Column(db.Integer, unique = False)
-    appointment_id = db.Column(db.Integer, unique = True)
+    appointment_id = db.Column(db.Integer, unique = False)
     notes = db.Column(db.String(3000), unique = False)
 
     def __init__(self, doctor_id, patient_id, appointment_id, notes):
@@ -108,4 +116,27 @@ class MedicalRecordSchema(ma.Schema):
 
 medical_report_schema = AppointmentSchema()
 medical_reports_schema = AppointmentSchema(many = True)
+
+class Availability(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    doctor_id = db.Column(db.Integer, unique=False)
+    date = db.Column(db.Date, unique=False)
+    time_start = db.Column(db.Time, unique=False)
+    time_end = db.Column(db.Time, unique=False)
+    event_id = db.Column(db.String(120), unique=False)
+
+    def __init__(self, doctor_id, date, time_start, time_end, event_id):
+        self.doctor_id = doctor_id
+        self.date = date
+        self.time_start = time_start
+        self.time_end = time_end
+        self.event_id = event_id
+
+class AvailabilitySchema(ma.Schema):
+    class Meta:
+        fields = ('doctor_id','date','time_start','time_end', 'event_id')
+
+availability_schema = AvailabilitySchema()
+availability_schema = AvailabilitySchema(many = True)
+
 
--- smartoffice/smartoffice/doctor/app.py
+++ smartoffice/smartoffice/doctor/app.py
@@ -1,14 +1,20 @@
 from flask import Blueprint
 from flask import Flask, render_template, session, url_for, redirect, request
-import time, datetime
+import time, datetime, json
 import sys
+# Pi's directory
 # sys.path.insert(0,'/home/pi/A2/smartoffice/smartoffice/')
-sys.path.insert(0,'/Users/User/Downloads/smartoffice/smartofficeA2/smartoffice/smartoffice')
+# Bram's directory
+sys.path.insert(0,'/Users/BramanthaPatra/A2Git/smartofficeA2/smartoffice/smartoffice')
+# April's directory 
+# sys.path.insert(0,'/Users/User/Downloads/smartoffice/smartofficeA2/smartoffice/smartoffice')
+
 mod = Blueprint('doctor',__name__, template_folder='templates')
 
 from smartoffice import api_caller
 
 appointments_html = "doctor_appointments.html"
+calendar_html = "doctor_calendar.html"
 
 def loginState():
     if 'type' in session:
@@ -48,7 +54,13 @@ def calendar():
 	if redirect_link != None:
 		return redirect(redirect_link)
 
-	return render_template('doctor_nav.html', content = 'doctor_calendar.html')
+	doctors = api_caller.get_doctor(int(session['id']))
+	data_output = {
+        'doctors': doctors,
+        'content':calendar_html
+    }
+
+	return render_template('doctor_nav.html', **data_output)
 
 # add an availability
 @mod.route('/add_availability', methods=['POST'])
@@ -62,28 +74,28 @@ def add_availability():
 	time_start = request.form['time_start']
 	time_end = request.form['time_end']
 	summary = request.form['doctor_name']
+	calendar_id = request.form['doctor_calendar_id']
 
-	event_id = api_caller.add_to_calendar(summary,doctor_id, date, time_start, time_end)
+	event_id = api_caller.add_to_calendar(summary,doctor_id, date, time_start, time_end, calendar_id)
 	api_caller.add_availability(doctor_id, date, time_start, time_end, event_id)
 
-	add_appointment_automatically(doctor_id, date, time_start, time_end)
+	add_appointment_automatically(doctor_id, date, time_start, time_end, calendar_id)
 
 	return redirect(url_for("doctor.availabilities"))
 
-def add_appointment_automatically(doctor_id, date, time_start, time_end):
+def add_appointment_automatically(doctor_id, date, time_start, time_end, calendar_id):
 	patient_id = None
 	summary = "Appointment"
 
 	st = datetime.datetime.strptime(time_start, '%H:%M')
 	et = datetime.datetime.strptime(time_end, '%H:%M')
 	tdelta = et - st
-	fifteen_mins = datetime.datetime.strptime('00:15', '%H:%M')
 
 	num = int((tdelta.total_seconds()/3600) * 4)
 	for x in range(num):
 		time_end = st + datetime.timedelta(minutes = 15)
 		time_end_format = datetime.datetime.strftime(time_end, '%H:%M')
-		event_id = api_caller.add_to_calendar(summary,doctor_id, date, time_start, time_end_format)
+		event_id = api_caller.add_to_calendar(summary,doctor_id, date, time_start, time_end_format, calendar_id)
 		api_caller.add_appointment(doctor_id, date, time_start, time_end_format, patient_id, event_id)
 
 		# time_end_strptime = datetime.datetime.strptime(time_end, '%Y %m %d %H:%M:%S')
@@ -100,9 +112,38 @@ def remove_availability():
     
     availability_id = request.form['availability_id']
     event_id = request.form['event_id']
+    doctor_id = request.form['doctor_id']
+    date = request.form['date']
+    time_start = request.form['time_start']
     api_caller.remove_availability(availability_id)
     api_caller.remove_from_calendar(event_id)
 
+    remove_appoinment_automatically(doctor_id, date, time_start)
+
     return redirect(url_for("doctor.availabilities"))
+
+
+def remove_appoinment_automatically(doctor_id, date, time_start):
+
+	appointments = api_caller.get_appointments()
+	appointments_dumps = json.dumps(appointments, default=lambda o: o.__dict__, sort_keys=True, indent=4)
+	appointments_loads = json.loads(appointments_dumps)
+
+
+	st = datetime.datetime.strptime(time_start, '%H:%M:%S')
+	time_start_format = datetime.datetime.strftime(st, '%H:%M:%S')
+
+
+	for app in appointments_loads:
+		if app['doctor_id'] == int(doctor_id):
+			if app['date'] == date:
+				if app['time_start'] == time_start_format:
+					api_caller.remove_appointment(app['id'])
+					st = st + datetime.timedelta(minutes = 15)
+					time_start_format = datetime.datetime.strftime(st, '%H:%M:%S')
+
+
+
+
 
 
