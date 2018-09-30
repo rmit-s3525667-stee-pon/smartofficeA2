38fc3395e83868387471d5819eb3af711c96c6e0
rmit-s3624420-TruongGiang-Le <s3624420@student.rmit.edu.au>
2018-09-30 21:55:21 +1000

    changes

--- run.py
+++
@@ -1,5 +0,0 @@
-from smartoffice import app
-from smartoffice import db
-app.secret_key = 'super secret key'
-app.config['SESSION_TYPE'] = 'filesystem'
-app.run(host = '0.0.0.0', port = 80 ,debug=True)
